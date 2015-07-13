35b2cc35adc305109554b082109a4226fc41b401
Bella <woo.bella@gmail.com>
2015-07-13 10:47:37 -0400

    with solutions

--- tests.rb
+++ tests.rb
@@ -6,8 +6,22 @@ class Student
     CSV.foreach "db.csv" do |row|
     end
   end
-end
 
+  attr_reader :id, :first_name, :test_average
+  def initialize attributes
+    @id, @first_name, @last_name, @absences, @quiz_average, @test_average = attributes
+  end
+
+  def self.without_absences
+    students = []
+    CSV.foreach "db.csv" do |row|
+      if row[3] == "0"
+        students.push row
+      end
+    end
+    students
+  end
+end
 class StudentTest < Minitest::Test
   def test_student_attributes
     s = Student.new([1, "Su", "Kim", 0, 99, 100])
@@ -17,10 +31,10 @@ class StudentTest < Minitest::Test
   end
 
   def test_can_find_student_with_best_tests
-    assert_equal __, Student.without_absences.count
+    assert_equal 0, Student.without_absences.count
   end
 
   def test_can_find_highest_scoring_student
-    assert_equal __, Student.top_by_test_score.first_name
+    assert_equal 14, Student.top_by_test_score.first_name
   end
 end
